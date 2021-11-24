361db1c6a5f1afa48dae08994bbbdbdc39dd2740
varun <varunprakash652@gmail.com>
2021-11-24 19:24:50 +0530

    Home Page Edit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ export default function header() {
   return (
     <div>
       <div className="header">
-        <div className="headertitles"></div>
+        <div className="headertitles">
+          <span className="headertitlesm">Personal</span>
+          <span className="headertitlelg">Blog</span>
+        </div>
+        <img
+          className="headerImg"
+          alt=""
+          src="https://images.pexels.com/photos/8984277/pexels-photo-8984277.jpeg?cs=srgb&dl=pexels-slava-mkrvsky-8984277.jpg&fm=jpg"
+        />
       </div>
     </div>
   );
